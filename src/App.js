f53418a5443d08273736982f4baab86985583d98
burd5 <austinburdette5@gmail.com>
2022-11-18 12:49:47 -0700

    changed home padding and egg size

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import Header from './components/Header'
-import Footer from './components/Footer'
 import Home from './components/Home'
 import './components/header.css'
 import './components/footer.css'
